e3531e208a5e8d4dc8b92251425df5119907bec2
simantaray <simantaray89@gmail.com>
2022-06-06 10:27:42 +0530

    add Header

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+
+export default function Header() {
+
+
+  return (
+      <header className="text-black bg-white drop-shadow-md">
+        <div className="relative h-20 flex items-center justify-between p-10">
+          {/* logo */}
+          <div className="cursor-pointer text-gray-900">
+          <img className="mx-auto h-12 w-auto" src="https://cdn.dribbble.com/users/434375/screenshots/16798272/media/eadadc738a83c444fea86fca0b385051.jpg?compress=1&resize=1200x900&vertical=top" alt="Workflow"></img>
+      
+          </div>
+          <button class="bg-indigo-600 hover:bg-indigo-700 p-2 text-white font-semibold rounded-md px-6">Sign Up</button>
+        </div>
+      </header>
+  );
+}
